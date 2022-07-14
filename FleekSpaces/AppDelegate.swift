c9521a86ab45fe5bac271cb3abcb6f86c9ca4e82
Mayur <mayur.patil@getfleek.app>
2022-07-14 11:07:27 +0530

    firebase chat fix

--- FleekSpaces/AppDelegate.swift
+++ FleekSpaces/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        FirebaseApp.configure()
+//        FirebaseApp.configure()
         Messaging.messaging().delegate = self
         UNUserNotificationCenter.current().delegate = self
         
